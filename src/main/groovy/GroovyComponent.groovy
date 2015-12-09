4916d682d204980a53a25356c14c362a4499762d
Bayo Erinle <berinle@gmail.com>
2015-12-09 10:38:20 -0500

    basic setup

---
+++ src/main/groovy/GroovyComponent.groovy
@@ -0,0 +1,16 @@
+class GroovyComponent {
+  def helloWorld() {
+    println 'hello world!'
+
+    def proc = ['/bin/zsh', '-c', '/usr/local/bin/docker-machine env dev'].execute()
+    def sout = new StringBuilder(), serr = new StringBuilder()
+
+    proc.waitForProcessOutput(sout, serr)
+
+    println sout
+    println "--------------------\n"
+    println serr
+
+    println 'done calling shell script'
+  }
+}
